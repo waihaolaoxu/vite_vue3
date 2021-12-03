e98b5261773382f7e21cd9cd763191cd149c3dc1
xushilong <xushilong@dyrs.com.cn>
2021-12-03 15:44:50 +0800

    update

--- src/views/a.vue
+++ src/views/a.vue
@@ -12,8 +12,12 @@
         <li>片段</li>
         <li>setup 语法糖</li>
     </ul>
+    <van-button type="primary">被assets/css/vant-theme重置过的样式</van-button>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+//使用全局scss变量
+li {
+    color: $color-logo
+}
 </style>
